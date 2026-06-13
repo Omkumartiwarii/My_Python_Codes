75ca98852628918c10494b6d30d735b03be1d92d
Om Kumar <omkumar00116@gmail.com>
2026-06-13 23:53:37 +0530

    Diff. approach

--- Basic Programs/uppercase & lowercase.py
+++ Basic Programs/uppercase & lowercase.py
@@ -1,3 +1,17 @@
+#approach 1: Using Built-in Functions
 word = input("Enter a string: ")
 print(f"Uppercase: {word.upper()}")
 print(f"Lowercase: {word.lower()}")
+
+
+#approach 2: Using Loop
+word = input("Enter a string: ")
+uppercase = ""
+lowercase = ""
+for char in word:
+    if char.isupper():
+        uppercase += char
+    elif char.islower():
+        lowercase += char
+print(f"Uppercase: {uppercase}")
+print(f"Lowercase: {lowercase}")
