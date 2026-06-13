3fdb3adf810f034457b3740b1d66130c0706da2b
Om Kumar <omkumar00116@gmail.com>
2026-06-13 23:54:01 +0530

    Diff. approach

--- Basic Programs/Word count.py
+++ Basic Programs/Word count.py
@@ -1,4 +1,16 @@
+#Word count in String
+
+#Approach 1: Using Built-in Functions
 word = input("Enter a string: ")
 word_list = word.split()
 word_count = len(word_list)
+print(f"Number of words in the string: {word_count}")
+
+
+#Approach 2: Using Loop
+word = input("Enter a string: ")
+word_list = word.split()
+word_count = 0
+for i in word_list:
+    word_count += 1
 print(f"Number of words in the string: {word_count}")
