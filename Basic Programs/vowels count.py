ed76514b512b498606437b7fcb2b6e4c465b4249
Om Kumar <omkumar00116@gmail.com>
2026-06-13 23:55:59 +0530

    Diff. approach

--- Basic Programs/vowels count.py
+++ Basic Programs/vowels count.py
@@ -6,3 +6,10 @@ for char in string: #char is a variable that takes each character of the string 
     if char in vowels:
         count += 1
 print(f"Number of vowels in the string: {count}")
+
+
+#Approach 2: Using Built-in Functions
+string = input("Enter a string: ")
+vowels = "aeiouAEIOU"
+count = sum(1 for char in string if char in vowels)
+print(f"Number of vowels in the string: {count}")
