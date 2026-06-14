eb15736940159339b4fa244ccf5a037aa9d20ec5
Om Kumar <omkumar00116@gmail.com>
2026-06-14 23:58:41 +0530

    Anagram check

---
+++ Basic Programs/Anagram.py
@@ -0,0 +1,16 @@
+def anagram_check(s1,s2):
+    count = 0
+    if(sorted(s1.lower()) == sorted(s2.lower())):
+        count +=1
+    
+    return count
+
+word1 = input("Enter 1st word : ")
+word2 = input("Enter 2nd word : ")
+
+anagram = anagram_check(word1,word2)
+
+if anagram == 0:
+    print(f"\"{word1}\" and \"{word2}\" is not Anagram")
+else:
+    print(f"\"{word1}\" and \"{word2}\" is Anagram")
