ec67f5311cf6008ec861ff055a652b989a9350bf
Om Kumar <omkumar00116@gmail.com>
2026-06-15 00:45:57 +0530

    New Approach Added in duplicate remove program

--- Basic Programs/Duplicate characters remove.py
+++ Basic Programs/Duplicate characters remove.py
@@ -12,3 +12,15 @@ for ch in word:
 print(result)
 
 
+#Approach 2: Using Dictionary
+word = input("Enter a string: ")
+
+char_freq = {}
+
+for ch in word:
+    if ch not in char_freq:
+        char_freq[ch] = 1
+
+print("".join(char_freq.keys()))
+
+
